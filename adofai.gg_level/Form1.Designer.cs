6db9d02808643e7cad4115650212f5586014e578
leo82380 <tkdrb062@gmail.com>
2023-04-19 09:10:49 +0900

    디자인 수정

--- adofai.gg_level/Form1.Designer.cs
+++ adofai.gg_level/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace adofai.gg_level
             this.levelBtn = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.levelcur = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // levelBtn
@@ -65,19 +66,33 @@ namespace adofai.gg_level
             this.levelcur.Font = new System.Drawing.Font("godoMaum", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.levelcur.Location = new System.Drawing.Point(45, 211);
             this.levelcur.Name = "levelcur";
+            this.levelcur.ReadOnly = true;
             this.levelcur.Size = new System.Drawing.Size(370, 68);
             this.levelcur.TabIndex = 3;
+            this.levelcur.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("godoMaum", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(45, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 76);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "사용법";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(466, 362);
+            this.ClientSize = new System.Drawing.Size(466, 411);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.levelcur);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.levelBtn);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "얼불춤 레벨 추천";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,6 +103,7 @@ namespace adofai.gg_level
         private System.Windows.Forms.Button levelBtn;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox levelcur;
+        private System.Windows.Forms.Button button1;
     }
 }
 
